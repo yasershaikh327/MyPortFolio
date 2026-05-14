e14ed237eb2da4d25784344d6c13caf2403b1105
Yaser Shaikh <syaser327@gmail.com>
2026-05-14 23:07:47 +0530

    asd

--- api/viewer.js
+++ api/viewer.js
@@ -141,7 +141,7 @@ export default async function handler(req, res) {
     }
 }
 
+console.error("teswt");
 console.log("ENV CHECK:", {
-  API_URL: process.env.API_URL,
   DATABASE_URL: process.env.DATABASE_URL
 });
