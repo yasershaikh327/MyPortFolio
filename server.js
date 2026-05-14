578856fcee44f78948b88787c6a22e31cff94c57
Yaser Shaikh <syaser327@gmail.com>
2026-05-14 23:56:02 +0530

    asdasdas

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const YOUR_API_V3_KEY = process.env.BREVO_API_KEY || '';
 
 // Middleware
 app.use(cors());
